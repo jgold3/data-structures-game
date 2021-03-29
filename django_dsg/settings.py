c29deac38c10782883581963b2064fd6de4370b6
jgold3 <32280330+jgold3@users.noreply.github.com>
2021-03-29 16:58:54 -0400

    Changed back

--- django_dsg/settings.py
+++ django_dsg/settings.py
@@ -68,7 +68,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'public')
+            os.path.join(BASE_DIR, 'build')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
